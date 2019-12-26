ad2bc7765238142b2e8145a0d274966e5e2aed32
verifyCode <HelloMrXu@foxmail.com>
2019-12-26 09:56:40 +0800

    q104

--- src/main/java/com/q05longest_palindromic_substring/Solution.java
+++ src/main/java/com/q05longest_palindromic_substring/Solution.java
@@ -1,6 +1,8 @@
 package com.q05longest_palindromic_substring;
 
 /**
+ * @author xjn
+ * @since 2019-12-23
  * https://leetcode-cn.com/problems/longest-palindromic-substring/
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  * 输入: "babad"
@@ -8,13 +10,18 @@ package com.q05longest_palindromic_substring;
  * 注意: "aba" 也是一个有效答案。
  * 输入: "cbbd"
  * 输出: "bb"
- *
- * @author xjn
- * @since 2019-12-23
+ * <p>
+ * <p>
  * 马拉车算法:https://www.jianshu.com/p/392172762e55
  */
 public class Solution {
     public String longestPalindrome(String s) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        System.out.println(test.longestPalindrome("babad"));
+        System.out.println(test.longestPalindrome("cbbd"));
     }
 }
