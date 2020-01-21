4516fee01fb31ccbd6baf85e47459ec9ef7e5562
verifyCode <HelloMrXu@foxmail.com>
2020-01-21 17:34:50 +0800

    q350

---
+++ src/main/java/com/q18_4sum/Solution.java
@@ -0,0 +1,75 @@
+package com.q18_4sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author xjn
+ * @since 2020-01-21
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ * <p>
+ * 注意：
+ * <p>
+ * 答案中不可以包含重复的四元组。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ * <p>
+ * 满足要求的四元组集合为：
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/4sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//TODO
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+//        Arrays.sort(nums);
+//        List<List<Integer>> res = new ArrayList<>();
+//        for (int i = 0; i < nums.length - 2; i++) {
+//            int l = i + 2;
+//            int r = nums.length - 1;
+//            int sum = nums[i] + nums[i + 1];
+//            while (l < r) {
+//                List<Integer> list = new ArrayList<>();
+//                if (nums[l] + nums[r] + sum == target) {
+//                    list.add(nums[l]);
+//                    list.add(nums[r]);
+//                    list.add(nums[i]);
+//                    list.add(nums[i + 1]);
+//                    while (l < r && nums[l] == nums[l + 1]) {
+//                        l++;
+//                    }
+//                    while (l < r && nums[r] == nums[r - 1]) {
+//                        r--;
+//                    }
+//                    l++;
+//                    r--;
+//                    res.add(list);
+//                } else if (nums[l] + nums[r] + sum > target) {
+//                    r--;
+//                } else {
+//                    l++;
+//                }
+//            }
+//        }
+//        return res;
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> lists = test.fourSum(nums, target);
+        System.out.println(lists);
+    }
+
+}
---
+++ src/main/java/com/q350_intersection_of_two_arrays_ii/Solution.java
@@ -0,0 +1,53 @@
+package com.q350_intersection_of_two_arrays_ii;
+
+import java.util.*;
+
+/**
+ * @author xjn
+ * @since 2020-01-21
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ */
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            Integer count = map.get(nums1[i]);
+            if (count == null) {
+                count = 1;
+            } else {
+                count++;
+            }
+            map.put(nums1[i], count);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            Integer count = map.get(nums2[i]);
+            if (count != null && count > 0) {
+                res.add(nums2[i]);
+                count--;
+                map.put(nums2[i], count);
+            }
+        }
+        int[] resArray = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            resArray[i] = res.get(i);
+        }
+        return resArray;
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        int[] nums = new int[]{4, 9, 5};
+        int[] nums2 = new int[]{9, 4, 9, 8, 4};
+        int[] intersect = test.intersect(nums, nums2);
+        System.out.println(Arrays.toString(intersect));
+    }
+}
