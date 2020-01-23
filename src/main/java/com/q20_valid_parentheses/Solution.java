2c9b025cba35235c3543533e9b536f07524364f6
verifyCode <HelloMrXu@foxmail.com>
2020-01-23 12:14:38 +0800

    2020年01月23日12:14:37

---
+++ src/main/java/com/q20_valid_parentheses/Solution.java
@@ -0,0 +1,43 @@
+package com.q20_valid_parentheses;
+
+import java.util.Stack;
+
+/**
+ * @author xjn
+ * @since 2020-01-23
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty()) {
+                    Character pop = stack.pop();
+                    if (pop == '(' && ch != ')') {
+                        return false;
+                    }
+                    if (pop == '[' && ch != ']') {
+                        return false;
+                    }
+                    if (pop == '{' && ch != '}') {
+                        return false;
+                    }
+                }else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+//        System.out.println(test.isValid("()"));
+//        System.out.println(test.isValid("()[]{}"));
+        System.out.println(test.isValid("{[()]}"));
+    }
+}
---
+++ src/main/java/com/q24_swap_nodes_in_pairs/Solution.java
@@ -0,0 +1,23 @@
+package com.q24_swap_nodes_in_pairs;
+
+/**
+ * @author xjn
+ * @since 2020-01-23
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+//TODO
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+        System.out.println(test.swapPairs(head));
+    }
+}
