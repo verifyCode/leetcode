dfa130f01975d8535ce982d1aaaf609f88b7605d
verifyCode <HelloMrXu@foxmail.com>
2020-02-03 17:30:49 +0800

    最小路径和 & 三角形路径

---
+++ src/main/java/com/q120_triangle/Solution2.java
@@ -0,0 +1,65 @@
+package com.q120_triangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xjn
+ * @since 2020-02-03
+ * * https://leetcode-cn.com/problems/triangle/
+ * * 120. 三角形最小路径和
+ */
+public class Solution2 {
+
+    private int m;
+
+    private Integer[][] ans;
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        m = triangle.size();
+        ans = new Integer[m][m];
+        return minimumTotal(triangle, 0, 0);
+    }
+
+    public int minimumTotal(List<List<Integer>> list, int x, int y) {
+        if (ans[x][y] != null) {
+            return ans[x][y];
+        }
+        if (x == m - 1) {
+            return list.get(x).get(y);
+        }
+
+        int left = minimumTotal(list, x + 1, y);
+        int right = minimumTotal(list, x + 1, y + 1);
+        ans[x][y] = Math.min(left, right) + list.get(x).get(y);
+        return ans[x][y];
+    }
+
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(2);
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(3);
+        l2.add(4);
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(6);
+        l3.add(5);
+        l3.add(7);
+        List<Integer> l4 = new ArrayList<>();
+        l4.add(4);
+        l4.add(1);
+        l4.add(8);
+        l4.add(3);
+        lists.add(l1);
+        lists.add(l2);
+        lists.add(l3);
+        lists.add(l4);
+        System.out.println(solution.minimumTotal(lists));
+    }
+}
---
+++ src/main/java/com/q64_minimum_path_sum/Solution2.java
@@ -0,0 +1,49 @@
+package com.q64_minimum_path_sum;
+
+/**
+ * @author xjn
+ * @since 2020-02-03
+ * https://leetcode-cn.com/problems/minimum-path-sum/
+ * 64. 最小路径和
+ */
+public class Solution2 {
+
+    private int m, n;
+    private int[][] ans;
+
+    public int minPathSum(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        ans = new int[m][n];
+        return minPathSum(grid, m - 1, n - 1);
+    }
+
+    private int minPathSum(int[][] grid, int x, int y) {
+        if (x < 0 || y < 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (x == 0 && y == 0) {
+            return grid[x][y];
+        }
+        if (ans[x][y] > 0) {
+            return ans[x][y];
+        }
+        if (inArea(x, y)) {
+            int up = minPathSum(grid, x - 1, y);
+            int left = minPathSum(grid, x, y - 1);
+            ans[x][y] = Math.min(up, left) + grid[x][y];
+        }
+        return ans[x][y];
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        System.out.println(solution.minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}));
+    }
+}
+
