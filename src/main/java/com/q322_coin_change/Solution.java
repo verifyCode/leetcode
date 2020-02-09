017da6e9bdd416b78fa6c9854b77a2cc0257ee66
verifyCode <HelloMrXu@foxmail.com>
2020-02-09 21:07:33 +0800

    q322 零钱兑换

--- src/main/java/com/q322_coin_change/Solution.java
+++ src/main/java/com/q322_coin_change/Solution.java
@@ -1,5 +1,7 @@
 package com.q322_coin_change;
 
+import java.util.HashMap;
+
 /**
  * @author xjn
  * @since 2020-02-08
@@ -8,25 +10,44 @@ package com.q322_coin_change;
  */
 public class Solution {
 
+    private int[] memo;
+    private int maxAmount;
+
     public int coinChange(int[] coins, int amount) {
-        return coinChange(coins, amount, coins.length - 1);
+        if (coins == null || coins.length == 0) {
+            return -1;
+        }
+        memo = new int[amount + 1];
+        maxAmount = amount + 1;
+        int a = coinChangeR(coins, amount);
+        return a == maxAmount ? -1 : a;
     }
 
-    private int coinChange(int[] coins, int amount, int index) {
-        if (index < 0) {
-            return 0;
+    private int coinChangeR(int[] coins, int amount) {
+        if (memo[amount] != 0) {
+            return memo[amount];
         }
         if (amount == 0) {
-            return 1;
+            return 0;
         }
-        return 1;
-
+        int res = maxAmount;
+        for (int i = 0; i < coins.length; i++) {
+            if (amount - coins[i] >= 0) {
+                int a = coinChangeR(coins, amount - coins[i]) + 1;
+                res = Math.min(res, a);
+            }
+        }
+        memo[amount] = res;
+        return res;
     }
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
         System.out.println(solution.coinChange(new int[]{1, 2, 5}, 11));
+        System.out.println(solution.coinChange(new int[]{2}, 3));
+        System.out.println(solution.coinChange(new int[]{1}, 0));
+        System.out.println(solution.coinChange(new int[]{0}, 0));
     }
 
 
---
+++ src/main/java/com/q322_coin_change/Solution3.java
@@ -0,0 +1,41 @@
+package com.q322_coin_change;
+
+import java.util.Arrays;
+
+/**
+ * @author xjn
+ * @since 2020-02-09
+ * https://leetcode-cn.com/problems/coin-change/
+ * 322. 零钱兑换
+ */
+public class Solution3 {
+    //dp[i] 在区间coin[0~n]中兑换i元钱最少需要多少硬币
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+//        for (int i = 1; i < coins.length; i++) {
+//            for (int j = 0; j <= amount; j++) {
+//                if (j - coins[i] >= 0) {
+//                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+//                }
+//            }
+//        }
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        Solution3 solution = new Solution3();
+        System.out.println(solution.coinChange(new int[]{1, 2, 5}, 11));//3
+        System.out.println(solution.coinChange(new int[]{2}, 3));//-1
+        System.out.println(solution.coinChange(new int[]{474, 83, 404, 3}, 264));//8
+    }
+}
