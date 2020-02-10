91577ee82c98d2fc79632cda873c33d650f19d2b
verifyCode <HelloMrXu@foxmail.com>
2020-02-10 21:14:38 +0800

    2020年02月10日21:14:36

--- src/main/java/com/q101_symmetric_tree/TreeNode.java
+++ src/main/java/com/q101_symmetric_tree/TreeNode.java
@@ -8,4 +8,8 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
 }
---
+++ src/main/java/com/q95_unique_binary_search_trees_ii/Solution.java
@@ -0,0 +1,42 @@
+package com.q95_unique_binary_search_trees_ii;
+
+import com.q101_symmetric_tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xjn
+ * @since 2020-02-10
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
+ * 95. 不同的二叉搜索树 II
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return dfs(1, n);
+    }
+
+    private List<TreeNode> dfs(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftList = dfs(start, i - 1);
+            List<TreeNode> rightList = dfs(i + 1, end);
+            for (TreeNode l : leftList) {
+                for (TreeNode r : rightList) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
