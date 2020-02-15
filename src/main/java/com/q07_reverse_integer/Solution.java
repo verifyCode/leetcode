8dbcab31f4f2f6b3e9d6b344a4997dc9b8fd1c88
verifyCode <HelloMrXu@foxmail.com>
2020-02-15 16:29:37 +0800

    回文数;

---
+++ src/main/java/com/q07_reverse_integer/Solution.java
@@ -0,0 +1,31 @@
+package com.q07_reverse_integer;
+
+/**
+ * @author xjn
+ * @since 2020-02-15
+ * https://leetcode-cn.com/problems/reverse-integer/
+ * 7. 整数反转
+ *
+ * 时间复杂度 O(log10^x)
+ * 空间复杂度 O(1)
+ */
+public class Solution {
+    public int reverse(int x) {
+        long sum = 0;
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+            if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int)sum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(123));
+        System.out.println(solution.reverse(-123));
+        System.out.println(solution.reverse(-120));
+    }
+}
---
+++ src/main/java/com/q09_palindrome_number/Solution.java
@@ -0,0 +1,34 @@
+package com.q09_palindrome_number;
+
+/**
+ * @author xjn
+ * @since 2020-02-15
+ * https://leetcode-cn.com/problems/palindrome-number/
+ * 9. 回文数
+ * 时间复杂度 O(log10^n)
+ * 空间复杂度O(1)
+ */
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || x != 0 && x % 10 == 0) {
+            return false;
+        }
+        int sum = 0;
+        int source = x;
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return sum == source;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(-121));
+        System.out.println(solution.isPalindrome(10));
+        System.out.println(solution.isPalindrome(0));
+
+    }
+}
