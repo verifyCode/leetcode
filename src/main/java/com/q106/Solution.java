7bd30d842dbece8417231d04c3547d3448b2228d
xjn <HelloMrXu@foxmail.com>
2020-05-25 23:04:47 +0800

    合并冲突

--- src/main/java/com/q106/Solution.java
+++ src/main/java/com/q106/Solution.java
@@ -1,9 +1,8 @@
 package com.q106;
-
 import com.q101_symmetric_tree.TreeNode;
-
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * @author xjn
@@ -12,29 +11,29 @@ import java.util.Map;
  */
 public class Solution {
     private Map<Integer, Integer> map = new HashMap<>();
-
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (inorder == null || postorder == null) {
-            return null;
-        }
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
-    }
-
     //中序遍历:左根右
     //后续遍历:左右根
-    private TreeNode helper(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd) {
-        if (iStart > iEnd || pStart > pEnd) {
-            return null;
-        }
-        //
-        TreeNode root = new TreeNode(postorder[pEnd]);
-        int rootPos = map.get(postorder[pEnd]);
-        int rightNum = iEnd - rootPos;
-        root.left = helper(inorder, iStart, rootPos - 1, postorder, pStart, pEnd - rightNum - 1);
-        root.right = helper(inorder, rootPos + 1, iEnd, postorder, pEnd - rightNum, pEnd - 1);
-        return root;
-    }
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		if (inorder == null || postorder == null) {
+			return null;
+		}
+		for (int i = 0; i < inorder.length; i++) {
+			map.put(inorder[i], i);
+		}
+		return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+	}
+	//中序遍历:左根右
+	//后续遍历:左右根
+	private TreeNode helper(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd) {
+		if (iStart > iEnd || pStart > pEnd) {
+			return null;
+		}
+		//
+		TreeNode root = new TreeNode(postorder[pEnd]);
+		int rootPos = map.get(postorder[pEnd]);
+		int rightNum = iEnd - rootPos;
+		root.left = helper(inorder, iStart, rootPos - 1, postorder, pStart, pEnd - rightNum - 1);
+		root.right = helper(inorder, rootPos + 1, iEnd, postorder, pEnd - rightNum, pEnd - 1);
+		return root;
+	}
 }
