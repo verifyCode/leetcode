67d7e184289c791d1309e0f5f0d40ff3f4df5fac
verifyCode <HelloMrXu@foxmail.com>
2020-05-31 19:27:28 +0800

    q155 && q232

---
+++ src/main/java/com/q232/MyQueue.java
@@ -0,0 +1,70 @@
+package com.q232;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author xjn
+ * @since 2020-05-31
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ * 232. 用栈实现队列
+ */
+public class MyQueue {
+    /**
+     * Initialize your data structure here.
+     */
+    private Deque<Integer> stack1, stack2;
+
+    public MyQueue() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            throw new RuntimeException("Empty");
+        }
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        int res = stack2.pop();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return res;
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            throw new RuntimeException("Empty");
+        }
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        int res = stack2.peek();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return res;
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
