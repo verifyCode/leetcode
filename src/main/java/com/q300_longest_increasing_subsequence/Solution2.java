ebe001d37f406c4d3b8eeebb440a566860980cdc
verifyCode <HelloMrXu@foxmail.com>
2020-05-31 18:36:38 +0800

    q53

--- src/main/java/com/q300_longest_increasing_subsequence/Solution2.java
+++ src/main/java/com/q300_longest_increasing_subsequence/Solution2.java
@@ -16,17 +16,14 @@ public class Solution2 {
         }
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-
+        int res = 1;
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(dp[i], res);
                 }
             }
-        }
-        int res = 1;
-        for (int i = 0; i < nums.length; i++) {
-            res = Math.max(dp[i], res);
         }
         return res;
     }
