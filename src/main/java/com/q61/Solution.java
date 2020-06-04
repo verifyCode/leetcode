612bea3f73f8b5256eb08cd20bcc6768a5220319
xjn <HelloMrXu@foxmail.com>
2020-06-04 20:33:28 +0800

    update

---
+++ src/main/java/com/q61/Solution.java
@@ -0,0 +1,46 @@
+package com.q61;
+
+import com.q86_partition_list.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/rotate-list/
+ * 61. 旋转链表
+ * 时间复杂度O(n)
+ * 空间复杂度O(1)
+ */
+public class Solution {
+	public ListNode rotateRight(ListNode head, int k) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		int len = getLength(head);
+		k = k % len;
+		ListNode end = head;
+		for (int i = 0; i < k; i++) {
+			end = end.next;
+		}
+		ListNode start = head;
+		while (end.next != null) {
+			start = start.next;
+			end = end.next;
+		}
+		end.next = head;
+		head = start.next;
+		start.next = null;
+		return head;
+	}
+	//s-->n
+	//h = s.next ==> h --> n
+
+	//        s      end
+	//1-->2-->3-->4-->5-->null
+	//4-->5-->1-->2-->3-->null
+	private int getLength(ListNode head) {
+		int res = 0;
+		while (head != null) {
+			res++;
+			head = head.next;
+		}
+		return res;
+	}
+}
--- src/main/java/com/q83_remove_duplicates_from_sorted_list/Solution.java
+++ src/main/java/com/q83_remove_duplicates_from_sorted_list/Solution.java
@@ -17,11 +17,16 @@ package com.q83_remove_duplicates_from_sorted_list;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 时间复杂度O(n)
+ * 空间复杂度O(1)
  */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
             return head;
+        }
+        if (head.val == head.next.val) {
+            head = head.next;
         }
         head.next = deleteDuplicates(head.next);
         if (head.val == head.next.val) {
