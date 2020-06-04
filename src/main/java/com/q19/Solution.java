8f31aaf7cbab83d906ef5cea08c93ee8dc5e0350
xjn <HelloMrXu@foxmail.com>
2020-06-04 16:07:17 +0800

    update

---
+++ src/main/java/com/q19/Solution.java
@@ -0,0 +1,29 @@
+package com.q19;
+
+import com.q86_partition_list.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/submissions/
+ * 19. 删除链表的倒数第N个节点
+ */
+public class Solution {
+	public ListNode removeNthFromEnd(ListNode head, int n){
+		if(head == null){
+			return head;
+		}
+		ListNode p = head;
+		ListNode q = head;
+		while (p != null){
+			if(n < 0){
+				q = q.next;
+			}
+			p = p.next;
+			n --;
+		}
+		if(n == 0){
+			return head.next;
+		}
+		q.next = q.next.next;
+		return head;
+	}
+}
