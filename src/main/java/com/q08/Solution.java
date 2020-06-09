2293c0d17bcda3df5745a210ced3eb82bcebef79
verifyCode <HelloMrXu@foxmail.com>
2020-06-09 23:47:24 +0800

    updaye

--- src/main/java/com/q08/Solution.java
+++ src/main/java/com/q08/Solution.java
@@ -8,7 +8,6 @@ public class Solution {
     public int myAtoi(String str) {
         int n = 0;
         int sum = 0;
-        int i = 0;
         while (n < str.length()) {
             int cur = str.charAt(n) - '0';
             sum = sum * 10 + cur;
@@ -16,21 +15,6 @@ public class Solution {
         }
         return sum;
     }
-
-
-    public int pow10(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        int res = pow10(n / 2);
-        if (n % 2 == 0) {
-            return res * res;
-        } else {
-            return res * res * 10;
-        }
-
-    }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.myAtoi("123"));
